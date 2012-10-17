33859cc9bfab8f15820673ff597ff08a13e0e350
Tom Nightingale <tom@affinitybridge.com>
2012-10-17 12:14:31 -0700

    expose widget options, fix case on js include

--- js/widget.js
+++ js/widget.js
@@ -9,13 +9,12 @@
             var id = $(item).attr('id'),
                 options = settings.geofield_leaflet_widget[id].settings;
 
-            var map = L.map(id, {
-                    center: options.map.center,
-                    zoom: options.map.zoom,
-                    layers: [L.tileLayer(options.map.base_url)],
-                    widget: { attach: options.dest },
-                    select: true
-                });
+            L.Util.extend(options.map, {
+                layers: [L.tileLayer(options.map.base_url)],
+                widget: { attach: options.dest },
+            });
+
+            var map = L.map(id, options.map);
             map.widget.enable();
 
             // Serialize data and set input value on submit.
