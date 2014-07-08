ea65787c7dac1d463f11e619c157f85a182c9bd8
Peter Philipp <peter.philipp@cando-image.com>
2014-07-08 08:04:48 +0200

    Coding standard cleanup.

--- leaflet_widget.api.php
+++ leaflet_widget.api.php
@@ -1,12 +1,15 @@
 <?php
+/**
+ * @file
+ * API documentation of leaflet widget module for Geofield.
+ */
 
 /**
- * Example implementation for hook_leaflet_widget_base_layers().
  * Allows developers to declare base layers to be used by the widget.
  *
  * Return an array of base layer names keyed by the layer's URL template string.
  */
-function example_leaflet_widget_base_layers() {
+function hook_leaflet_widget_base_layers() {
   return array(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' => 'OSM Mapnik',
   );
