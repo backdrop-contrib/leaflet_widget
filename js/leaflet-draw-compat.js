0e7b8fa462c3981da448de36d395d7d24c5084e3
indigoxela <user@olga.indigofloat.at>
2022-10-10 13:37:51 +0200

    Issue #6: Add draw compatibility for recent Leaflet version

---
+++ js/leaflet-draw-compat.js
@@ -0,0 +1,25 @@
+/**
+ * @file
+ * Override methods from Leaflet.draw for compatibility with Leaflet 1.9+.
+ *
+ * Prevent deprecation warnings:
+ * "Deprecated use of _flat, please use L.LineUtil.isFlat instead."
+ * Deprecated since 2017, (will be) removed in 2.x.
+ */
+L.Edit.Poly.include({
+  _defaultShape: function () {
+    if (!L.LineUtil.isFlat) {
+      return this._latlngs;
+    }
+    return L.LineUtil.isFlat(this._poly._latlngs) ? this._poly._latlngs : this._poly._latlngs[0];
+  }
+});
+
+L.Edit.PolyVerticesEdit.include({
+  _defaultShape: function () {
+    if (!L.LineUtil.isFlat) {
+      return this._poly._latlngs;
+    }
+    return L.LineUtil.isFlat(this._latlngs) ? this._latlngs : this._latlngs[0];
+  }
+});
