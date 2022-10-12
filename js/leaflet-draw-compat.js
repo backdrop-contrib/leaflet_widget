f8ea25e10e2660a63ef24606972ea99c72df32a5
indigoxela <user@olga.indigofloat.at>
2022-10-12 12:59:08 +0200

    Issue #10: Update to really latest Leaflet.draw, prevent deprecation notice

--- js/leaflet-draw-compat.js
+++ js/leaflet-draw-compat.js
@@ -1,7 +1,9 @@
 /**
  * @file
  * Override methods from Leaflet.draw for compatibility with Leaflet 1.9+.
- *
+ */
+
+/**
  * Prevent deprecation warnings:
  * "Deprecated use of _flat, please use L.LineUtil.isFlat instead."
  * Deprecated since 2017, (will be) removed in 2.x.
@@ -21,5 +23,25 @@ L.Edit.PolyVerticesEdit.include({
       return this._latlngs;
     }
     return L.LineUtil.isFlat(this._latlngs) ? this._latlngs : this._latlngs[0];
+  }
+});
+
+/**
+ * Prevent deprecation warnings:
+ * "wrong event specified: touchleave"
+ * Also drop IE polyfill.
+ */
+L.Map.TouchExtend.include({
+  addHooks: function () {
+    L.DomEvent.on(this._container, 'touchstart', this._onTouchStart, this);
+    L.DomEvent.on(this._container, 'touchend', this._onTouchEnd, this);
+    L.DomEvent.on(this._container, 'touchmove', this._onTouchMove, this);
+    L.DomEvent.on(this._container, 'touchcancel', this._onTouchCancel, this);
+  },
+  removeHooks: function () {
+    L.DomEvent.off(this._container, 'touchstart', this._onTouchStart, this);
+    L.DomEvent.off(this._container, 'touchend', this._onTouchEnd, this);
+    L.DomEvent.off(this._container, 'touchmove', this._onTouchMove, this);
+    L.DomEvent.off(this._container, 'touchcancel', this._onTouchCancel, this);
   }
 });
